0e268026a3383098f5fc7dcf652faa8ec21d0c17
Enock Banda <enockbanda300@gmail.com>
2025-09-27 11:38:33 +0000

    hard code this url for realtime database cause this system should connec

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -11,11 +10,13 @@ const firebaseConfig = {
   projectId: "studio-4048203700-4cec7",
   storageBucket: "studio-4048203700-4cec7.appspot.com",
   messagingSenderId: "461026091475",
-  appId: "1:461026091475:web:9ea8ef2ff29a72f3c8cf26"
+  appId: "1:461026091475:web:9ea8ef2ff29a72f3c8cf26",
+  databaseURL: "https://studio-4048203700-4cec7-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getDatabase(app);
 
-export { app, auth };
+export { app, auth, db };
