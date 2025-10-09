959ba3280ac17c44bb82d848e13aba7ee6af9166
Enock Banda <enockbanda300@gmail.com>
2025-10-09 10:50:34 +0000

    check system defuet color on home page should be light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,8 +53,8 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
-            enableSystem
+            defaultTheme="light"
+            enableSystem={false}
             disableTransitionOnChange
         >
           <AuthProvider>
